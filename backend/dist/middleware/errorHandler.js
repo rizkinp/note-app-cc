81814a90da670992f7d526403a3acd3969c21706
rizkinp <rizkinp87@gmail.com>
2024-11-15 17:27:50 +0700

    fix:fix gitignore

--- backend/dist/middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const errorHandler = (err, req, res, next) => {
-    console.error(err);
-    res.status(500).json({
-        message: err.message || 'Internal Server Error',
-    });
-};
-exports.default = errorHandler;
