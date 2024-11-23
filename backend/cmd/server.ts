5bb260bfc928164b87435106131b06de9e959b5b
rizkinp <rizkinp87@gmail.com>
2024-11-23 20:27:33 +0700

    fix cors origin

--- backend/cmd/server.ts
+++ backend/cmd/server.ts
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', apiRoutes);
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://note-app-cc.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
